816f6d1c683062ec5603c1762731b7b4b82a95b5
msengbusch <marek.sengbusch@flg-online.de>
2022-07-06 12:43:28 +0200

    Revert "deineapp funktionier nie!!"
    This reverts commit d444bd9bce4f553f290e9a87f20280368f1ca25b.

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,4 +18,3 @@ dependencies {
     implementation("com.github.tinylog-org.tinylog:tinylog-core-modern:v3.0-SNAPSHOT")
     implementation("com.github.tinylog-org.tinylog:slf4j-tinylog:v3.0-SNAPSHOT")
 }
-//deieapp funktioniertnei!!!
