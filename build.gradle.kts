4dc3ee526efa401232054501bde561d85bc8385c
msengbusch <marek.sengbusch@flg-online.de>
2022-07-06 12:55:42 +0200

    build: use adoptium 18 jdk

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,13 @@ application {
     mainClass.set("de.localchat.Main")
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(18))
+        vendor.set(JvmVendorSpec.ADOPTOPENJDK)
+    }
+}
+
 dependencies {
     implementation("com.sparkjava:spark-core:2.9.3")
     implementation("com.github.tinylog-org.tinylog:tinylog-api:v3.0-SNAPSHOT")
