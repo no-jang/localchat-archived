ba1db3fceca11350d12481d28b5038eaae22b34b
msengbusch <66240817+msengbusch@users.noreply.github.com>
2022-07-06 14:54:00 +0200

    build: add netty transport as dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,13 @@ java {
 }
 
 dependencies {
+    // Web Server
     implementation("com.sparkjava:spark-core:2.9.3")
+
+    // Networking
+    implementation("io.netty:netty-transport:5.0.0.Alpha2")
+
+    // Logger
     implementation("com.github.tinylog-org.tinylog:tinylog-api:v3.0-SNAPSHOT")
     implementation("com.github.tinylog-org.tinylog:tinylog-impl:v3.0-SNAPSHOT")
     implementation("com.github.tinylog-org.tinylog:tinylog-core-modern:v3.0-SNAPSHOT")
