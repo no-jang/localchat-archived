7ef203e35d84298a74d77f021fdc27d62e02f2d1
msengbusch <marek.sengbusch@flg-online.de>
2022-07-06 12:39:02 +0200

    build: add tinylog as logger

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,8 @@ plugins {
 }
 
 repositories {
-    mavenCentral() 
+    mavenCentral()
+    maven("https://jitpack.io")
 }
 
 application {
@@ -12,4 +13,8 @@ application {
 
 dependencies {
     implementation("com.sparkjava:spark-core:2.9.3")
+    implementation("com.github.tinylog-org.tinylog:tinylog-api:v3.0-SNAPSHOT")
+    implementation("com.github.tinylog-org.tinylog:tinylog-impl:v3.0-SNAPSHOT")
+    implementation("com.github.tinylog-org.tinylog:tinylog-core-modern:v3.0-SNAPSHOT")
+    implementation("com.github.tinylog-org.tinylog:slf4j-tinylog:v3.0-SNAPSHOT")
 }
