c80068324c98c1dda44e9dcc257287e624e56662
msengbusch <66240817+msengbusch@users.noreply.github.com>
2022-07-08 23:46:38 +0200

    feat: failed multicast test

--- src/main/java/de/localchat/Test.java
+++ src/main/java/de/localchat/Test.java
@@ -1,7 +1,10 @@
 package de.localchat;
 
-import io.netty.channel.ChannelOption;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.InternetProtocolFamily;
+import io.netty.channel.socket.nio.NioDatagramChannel;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.netty.resources.LoopResources;
@@ -14,44 +17,48 @@ import java.util.Enumeration;
 import java.util.Random;
 
 public class Test {
-    public static void main(String[] args) throws IOException {
-        Random random = new Random();
-        int port = 44558;
-        InetSocketAddress multicastGroup = new InetSocketAddress("239.255.27.1", port);
+    private static class MulticastHandler extends  SimpleChannelInboundHandler<DatagramPacket> {
+        @Override
+        protected void channelRead0(ChannelHandlerContext ctx, DatagramPacket msg) throws Exception {
+            System.out.println("receive");
+        }
+    }
 
-        NetworkInterface multicastInterface = Collections.list(NetworkInterface.getNetworkInterfaces())
-                        .stream().filter(networkInterface -> {
-                    try {
-                        return networkInterface.isUp() && networkInterface.supportsMulticast();
-                    } catch (SocketException e) {
-                        throw new RuntimeException(e);
-                    }
-                })
-                .findFirst()
-                .orElseThrow();
+    public static void main(String[] args) {
+        InetSocketAddress groupAddress = new InetSocketAddress("239.255.27.1", 1234);
 
-        LoopResources resources = LoopResources.create("loop");
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            NetworkInterface ni = NetworkInterface.getByName("enp4s0");
+            Enumeration<InetAddress> addresses = ni.getInetAddresses();
+            InetAddress localAddress = null;
+            while (addresses.hasMoreElements()) {
+                InetAddress address = addresses.nextElement();
+                if (address instanceof Inet4Address){
+                    localAddress = address;
+                }
+            }
 
-        UdpServer.create()
-                //.option(ChannelOption.SO_BROADCAST, true)
-                .option(ChannelOption.SO_REUSEADDR, true)
-                .bindAddress(() -> multicastGroup)
-                .runOn(resources, InternetProtocolFamily.IPv4)
-                .handle((in, out) -> {
-                    in.join(multicastGroup.getAddress(), multicastInterface);
-                    in.receive().log().subscribe(byteBuf -> System.out.println("ue"));
-                    return Flux.never();
-                })
-                .bind()
-                .block();
+            Bootstrap b = new Bootstrap()
+                    .group(group)
+                    .channelFactory(() -> new NioDatagramChannel(InternetProtocolFamily.IPv4))
+                    .localAddress(localAddress, groupAddress.getPort())
+                    .option(ChannelOption.IP_MULTICAST_IF, ni)
+                    .option(ChannelOption.SO_REUSEADDR, true)
+                    .handler(new ChannelInitializer<NioDatagramChannel>() {
+                        @Override
+                        public void initChannel(NioDatagramChannel ch) throws Exception {
+                            ch.pipeline().addLast(new MulticastHandler());
+                        }
+                    });
 
-        MulticastSocket multicast = new MulticastSocket();
-        multicast.joinGroup(multicastGroup, multicastInterface);
-
-        byte[] data = new byte[1024];
-        random.nextBytes(data);
-
-        multicast.send(new DatagramPacket(data, data.length, multicastGroup.getAddress(), port));
-        multicast.close();
+            NioDatagramChannel ch = (NioDatagramChannel)b.bind(groupAddress.getPort()).sync().channel();
+            ch.joinGroup(groupAddress, ni).sync();
+            ch.closeFuture().await();
+        } catch (InterruptedException | SocketException e) {
+            e.printStackTrace();
+        } finally {
+            group.shutdownGracefully();
+        }
     }
 }
