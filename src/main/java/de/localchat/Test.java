8e0421c338d3e6fed179ae95cc948240d63a2c2d
msengbusch <66240817+msengbusch@users.noreply.github.com>
2022-07-09 10:39:38 +0200

    feat: test

--- src/main/java/de/localchat/Test.java
+++ src/main/java/de/localchat/Test.java
@@ -1,6 +1,11 @@
 package de.localchat;
 
-import io.netty.channel.ChannelOption;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.epoll.EpollDatagramChannel;
+import io.netty.channel.epoll.EpollEventLoopGroup;
+import io.netty.channel.socket.DatagramChannel;
 import io.netty.channel.socket.DatagramPacket;
 import io.netty.channel.socket.InternetProtocolFamily;
 import reactor.core.publisher.Flux;
@@ -11,35 +16,39 @@ import reactor.netty.resources.LoopResources;
 import reactor.netty.udp.UdpClient;
 import reactor.netty.udp.UdpServer;
 
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
 
 public class Test {
-    public static void main(String[] args) {
-        UdpServer.create()
-                .bindAddress(() -> new InetSocketAddress("255.255.255.255", 4567))
-                .option(ChannelOption.SO_BROADCAST, true)
-                .option(ChannelOption.SO_REUSEADDR, true)
-                .handle((in, out) -> {
-                    in.receive()
-                            .log()
-                            .subscribe(byteBuf -> System.out.println("Server received"));
-                    return Flux.never();
-                })
-                .bindNow();
+    public static final String MULTICAST_ADDRESS = "224.0.2.60";
+    public static final int MULTICAST_PORT = 4445;
 
-        Connection clientConnection = UdpClient.create()
-                //.runOn(LoopResources.create("udp"), InternetProtocolFamily.IPv4)
-                //.option(ChannelOption.SO_BROADCAST, true)
-                .option(ChannelOption.SO_REUSEADDR, true)
-                .handle((in, out) -> {
-                    in.receive()
-                            .log()
-                            .subscribe(byteBuf -> System.out.println("Client received"));
-                    //return out.sendString(Mono.just("Hello! "));
-                    return Flux.never();
-                })
-                .connectNow();
+    public static class ServerMulticastHandler extends SimpleChannelInboundHandler<DatagramPacket> {
+        @Override
+        protected void channelRead0(ChannelHandlerContext ctx, DatagramPacket msg) throws Exception {
+            System.out.println(msg.toString());
+        }
+    }
 
-        clientConnection.outbound().sendString(Mono.just("Hello!"));
+    public static void main(String[] args) throws UnknownHostException, InterruptedException {
+        EventLoopGroup group = new EpollEventLoopGroup();
+        Bootstrap bootstrap = new Bootstrap()
+                .remoteAddress(MULTICAST_ADDRESS, MULTICAST_PORT)
+                .group(group)
+                .channel(EpollDatagramChannel.class)
+                .handler(new ChannelInitializer<DatagramChannel>() {
+                    @Override
+                    protected void initChannel(DatagramChannel ch) throws Exception {
+                        ch.pipeline().addLast(new ServerMulticastHandler());
+                    }
+                });
+
+        ChannelFuture future = bootstrap.connect();
+        future.channel().closeFuture().sync();
+
+        group.shutdownGracefully();
     }
 }
