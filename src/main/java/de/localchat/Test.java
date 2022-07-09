7fb99ff0bba8b109ce2f3f0eea9a6edc24914389
msengbusch <66240817+msengbusch@users.noreply.github.com>
2022-07-09 10:42:55 +0200

    feat: working netty udp server example

--- src/main/java/de/localchat/Test.java
+++ src/main/java/de/localchat/Test.java
@@ -2,12 +2,15 @@ package de.localchat;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.*;
 import io.netty.channel.epoll.EpollDatagramChannel;
 import io.netty.channel.epoll.EpollEventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.DatagramChannel;
 import io.netty.channel.socket.DatagramPacket;
 import io.netty.channel.socket.InternetProtocolFamily;
+import io.netty.channel.socket.nio.NioDatagramChannel;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.netty.ByteBufMono;
@@ -16,11 +19,9 @@ import reactor.netty.resources.LoopResources;
 import reactor.netty.udp.UdpClient;
 import reactor.netty.udp.UdpServer;
 
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.SocketAddress;
-import java.net.UnknownHostException;
+import java.net.*;
 import java.nio.charset.StandardCharsets;
+import java.util.Enumeration;
 
 public class Test {
     public static final String MULTICAST_ADDRESS = "224.0.2.60";
@@ -29,26 +30,50 @@ public class Test {
     public static class ServerMulticastHandler extends SimpleChannelInboundHandler<DatagramPacket> {
         @Override
         protected void channelRead0(ChannelHandlerContext ctx, DatagramPacket msg) throws Exception {
-            System.out.println(msg.toString());
+            ByteBuf content = msg.content();
+            System.out.println(content.toString(StandardCharsets.UTF_8));
         }
     }
 
     public static void main(String[] args) throws UnknownHostException, InterruptedException {
-        EventLoopGroup group = new EpollEventLoopGroup();
-        Bootstrap bootstrap = new Bootstrap()
-                .remoteAddress(MULTICAST_ADDRESS, MULTICAST_PORT)
-                .group(group)
-                .channel(EpollDatagramChannel.class)
-                .handler(new ChannelInitializer<DatagramChannel>() {
-                    @Override
-                    protected void initChannel(DatagramChannel ch) throws Exception {
-                        ch.pipeline().addLast(new ServerMulticastHandler());
-                    }
-                });
+        InetSocketAddress groupAddress = new InetSocketAddress(MULTICAST_ADDRESS, MULTICAST_PORT);
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            NetworkInterface ni = NetworkInterface.getByName("enp4s0");
+            Enumeration<InetAddress> addresses = ni.getInetAddresses();
+            InetAddress localAddress = null;
+            while (addresses.hasMoreElements()) {
+                InetAddress address = addresses.nextElement();
+                if (address instanceof Inet4Address){
+                    localAddress = address;
+                }
+            }
 
-        ChannelFuture future = bootstrap.connect();
-        future.channel().closeFuture().sync();
+            Bootstrap b = new Bootstrap()
+                    .group(group)
+                    .channelFactory(new ChannelFactory<NioDatagramChannel>() {
+                        @Override
+                        public NioDatagramChannel newChannel() {
+                            return new NioDatagramChannel(InternetProtocolFamily.IPv4);
+                        }
+                    })
+                    .localAddress(localAddress, groupAddress.getPort())
+                    .option(ChannelOption.IP_MULTICAST_IF, ni)
+                    .option(ChannelOption.SO_REUSEADDR, true)
+                    .handler(new ChannelInitializer<NioDatagramChannel>() {
+                        @Override
+                        public void initChannel(NioDatagramChannel ch) throws Exception {
+                            ch.pipeline().addLast(new ServerMulticastHandler());
+                        }
+                    });
 
-        group.shutdownGracefully();
+            NioDatagramChannel ch = (NioDatagramChannel)b.bind(groupAddress.getPort()).sync().channel();
+            ch.joinGroup(groupAddress, ni).sync();
+            ch.closeFuture().await();
+        } catch (InterruptedException | SocketException e) {
+            e.printStackTrace();
+        } finally {
+            group.shutdownGracefully();
+        }
     }
 }
