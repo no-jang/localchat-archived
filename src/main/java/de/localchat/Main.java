61a7147832e6e721512600bff7dea4905de19c3a
msengbusch <66240817+msengbusch@users.noreply.github.com>
2022-07-10 21:26:49 +0200

    fix: remove discovery backend for short time

--- src/main/java/de/localchat/Main.java
+++ src/main/java/de/localchat/Main.java
@@ -1,11 +1,5 @@
 package de.localchat;
 
-import de.localchat.discovery.DiscoveryBackend;
-import de.localchat.discovery.DiscoveryListener;
-import de.localchat.discovery.DiscoveryRequest;
-import de.localchat.discovery.common.DefaultDiscoveryRequest;
-import de.localchat.discovery.udp.UDPDiscoveryBackend;
-
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -26,16 +20,6 @@ public class Main {
     private static int nextUserNumber = 1;
 
     public static void main(String[] args) throws IOException {
-        DiscoveryListener listener = new DiscoveryListener() {
-            @Override
-            public void receivedRequest(DiscoveryRequest request) {
-                System.out.println("Discovery received: " + request.getHost() + " web: " + request.getWebPort());
-            }
-        };
-
-        DiscoveryBackend backend = new UDPDiscoveryBackend(listener);
-        backend.broadcastRequest(new DefaultDiscoveryRequest("localhost", 78565));
-
         Javalin app = Javalin.create(config -> {
             config.addStaticFiles("/public", Location.CLASSPATH);
         }).start(7070);
