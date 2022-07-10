0cd3ef0551ca4bea556215f01b634a4edbc95da8
msengbusch <66240817+msengbusch@users.noreply.github.com>
2022-07-10 21:32:15 +0200

    build: fix dependency order

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,25 +34,14 @@ dependencies {
 
     // Kotlin
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.3")
-    //implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
 
-    // Web Server
+    // Web
     implementation("io.javalin:javalin:4.6.4")
-
-    //json
-    implementation("org.json:json:20220320")
-
-    //java to html
     implementation("com.j2html:j2html:1.5.0")
-
-    // Logger
-    implementation("com.github.tinylog-org.tinylog:tinylog-api:v3.0-SNAPSHOT")
-    implementation("com.github.tinylog-org.tinylog:tinylog-api-kotlin:v3.0-SNAPSHOT")
-    implementation("com.github.tinylog-org.tinylog:tinylog-impl:v3.0-SNAPSHOT")
-    implementation("com.github.tinylog-org.tinylog:slf4j-tinylog:v3.0-SNAPSHOT")
 
     // Data
     implementation("com.google.protobuf:protobuf-java:3.21.1")
+    implementation("org.json:json:20220320")
 
     // Network
     implementation("io.netty:netty5-handler:5.0.0.Alpha3")
@@ -61,6 +50,12 @@ dependencies {
     implementation("io.netty:netty5-transport-classes-kqueue:5.0.0.Alpha3")
     runtimeOnly("io.netty:netty5-transport-native-epoll:5.0.0.Alpha3:linux-x86_64")
     runtimeOnly("io.netty:netty5-transport-native-kqueue:5.0.0.Alpha3:osx-x86_64")
+
+    // Logger
+    implementation("com.github.tinylog-org.tinylog:tinylog-api:v3.0-SNAPSHOT")
+    implementation("com.github.tinylog-org.tinylog:tinylog-api-kotlin:v3.0-SNAPSHOT")
+    implementation("com.github.tinylog-org.tinylog:tinylog-impl:v3.0-SNAPSHOT")
+    implementation("com.github.tinylog-org.tinylog:slf4j-tinylog:v3.0-SNAPSHOT")
 
     // Testing
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.0-RC1")
