20adcff7b8915145a11a68fe5b344eb3ac0e8328
msengbusch <66240817+msengbusch@users.noreply.github.com>
2022-07-24 21:03:30 +0200

    style: fixed various style issues

--- gradle/build-logic-test/common/src/main/kotlin/common/Print.kt
+++ gradle/build-logic-test/common/src/main/kotlin/common/Print.kt
@@ -1,7 +1,27 @@
+/*
+ * Copyright (C) 2022 Nojang
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 package common
 
+/**
+ * Utility class to print a predefined message to the console.
+ */
 object Print {
-    fun print() {
+    /**
+     * Prints a message to the console.
+     */
+    fun printMessage() {
         println("Hello World")
     }
 }
