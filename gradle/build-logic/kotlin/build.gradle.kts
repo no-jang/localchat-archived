924581fcb2c81a16301fac42db5be08b47578b6e
msengbusch <66240817+msengbusch@users.noreply.github.com>
2022-07-24 12:10:43 +0200

    build(gradle): use detekt for static code analyses

--- gradle/build-logic/kotlin/build.gradle.kts
+++ gradle/build-logic/kotlin/build.gradle.kts
@@ -1,3 +1,5 @@
 dependencies {
+    // Kotlin
     implementation(libs.kotlin.gradle)
+    implementation(libs.kotlin.gradle.detekt)
 }
--- gradle/build-logic/kotlin/src/main/kotlin/build-logic.kotlin.gradle.kts
+++ gradle/build-logic/kotlin/src/main/kotlin/build-logic.kotlin.gradle.kts
@@ -1,6 +1,11 @@
 plugins {
     kotlin("jvm")
+
+    id("io.gitlab.arturbosch.detekt")
 }
+
+// TODO Wait for issue https://github.com/gradle/gradle/issues/15383 to be fixed
+val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
 
 repositories {
     mavenCentral()
@@ -10,5 +15,11 @@ repositories {
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(18))
+    }
+}
+
+dependencies {
+    detektPlugins(libs.findLibrary("kotlin.gradle.detekt.formatting").get()) {
+        exclude("org.slf4j", "slf4j-nop")
     }
 }
