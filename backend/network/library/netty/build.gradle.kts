609cfe59006d09e4ce6459a235159c0734e43d49
msengbusch <66240817+msengbusch@users.noreply.github.com>
2022-08-08 16:34:49 +0200

    fix(network-netty): use the non-empty native netty artifacts

--- backend/network/library/netty/build.gradle.kts
+++ backend/network/library/netty/build.gradle.kts
@@ -5,7 +5,9 @@ plugins {
 dependencies {
     api(projects.backend.network.library)
     implementation(libs.bundles.netty)
-    implementation(libs.bundles.netty.native)
+
+    runtimeOnly(variantOf(libs.netty.native.epoll) { classifier("linux-x86_64") })
+    runtimeOnly(variantOf(libs.netty.native.kqueue) { classifier("osx-x86_64") })
 
     testImplementation(testFixtures(projects.backend.common))
 }
