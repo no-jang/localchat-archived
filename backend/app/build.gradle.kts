b18bb7de7f17d2fe41eb540567780f347db49aff
msengbusch <66240817+msengbusch@users.noreply.github.com>
2022-08-10 13:06:08 +0200

    feat(network): add grpc

--- backend/app/build.gradle.kts
+++ backend/app/build.gradle.kts
@@ -1,7 +1,12 @@
+import com.google.protobuf.gradle.*
+
 plugins {
     id("build-logic.kotlin")
 
     application
+    idea
+
+    id("com.google.protobuf") version "0.8.13"
 }
 
 application {
@@ -9,5 +14,36 @@ application {
 }
 
 dependencies {
+    implementation(projects.backend.common)
     implementation(projects.backend.network.netty)
+
+    implementation("io.grpc:grpc-kotlin-stub:1.3.0")
+    implementation("io.grpc:grpc-protobuf:1.48.1")
+    implementation("io.grpc:grpc-netty:1.48.1")
+    implementation("com.google.protobuf:protobuf-kotlin:3.21.4")
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.21.2"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.48.1"
+        }
+        id("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:1.3.0:jdk8@jar"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                id("grpc")
+                id("grpckt")
+            }
+            it.builtins {
+                id("kotlin")
+            }
+        }
+    }
 }
--- backend/app/src/main/kotlin/de/localchat/app/Main.kt
+++ backend/app/src/main/kotlin/de/localchat/app/Main.kt
@@ -1,10 +1,39 @@
 package de.localchat.app
 
-import de.localchat.network.netty.hold.NettyEnvironmentFactory
+import GreeterGrpcKt
+import helloRequest
+import io.grpc.ManagedChannelBuilder
+import io.grpc.Server
+import io.grpc.ServerBuilder
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-fun main() {
-    val factory = NettyEnvironmentFactory()
-    val environment = factory.newEnvironment()
+suspend fun main() {
+    val port = 4568
 
-    println(environment.getName())
+    val server: Server = ServerBuilder
+        .forPort(4568)
+        .addService(HelloWorldService())
+        .build()
+
+    server.start()
+
+    val channel = ManagedChannelBuilder
+        .forAddress("localhost", port)
+        .usePlaintext()
+        .build()
+
+    val stub = GreeterGrpcKt.GreeterCoroutineStub(channel)
+    val request = helloRequest { name = "World" }
+
+    coroutineScope {
+        launch {
+            val response = stub.sayHello(request)
+            println("Greeting: ${response.message}")
+            delay(5000)
+        }
+    }
+
+    server.awaitTermination()
 }
