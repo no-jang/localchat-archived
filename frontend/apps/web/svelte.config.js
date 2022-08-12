56c052c507a5563fb88033dae4725376ca9cdda9
msengbusch <66240817+msengbusch@users.noreply.github.com>
2022-08-12 11:28:11 +0200

    style(frontend/app): reformat all

--- frontend/apps/web/svelte.config.js
+++ frontend/apps/web/svelte.config.js
@@ -3,21 +3,21 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter(),
-		prerender: {
-			default: true,
-		},
+  kit: {
+    adapter: adapter(),
+    prerender: {
+      default: true
+    },
 
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
-	}
+    // Override http methods in the Todo forms
+    methodOverride: {
+      allowed: ['PATCH', 'DELETE']
+    }
+  }
 };
 
 export default config;
