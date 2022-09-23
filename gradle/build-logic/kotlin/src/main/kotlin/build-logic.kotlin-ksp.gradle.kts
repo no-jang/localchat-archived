f4ae2b8b6de85e2831896004b1ccc2f3e34320df
msengbusch <66240817+msengbusch@users.noreply.github.com>
2022-09-24 00:41:49 +0200

    fix(gradle): idea uses ksp generated sources

--- gradle/build-logic/kotlin/src/main/kotlin/build-logic.kotlin-ksp.gradle.kts
+++ gradle/build-logic/kotlin/src/main/kotlin/build-logic.kotlin-ksp.gradle.kts
@@ -1,11 +1,20 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    id("build-logic.kotlin")
     id("com.google.devtools.ksp")
+}
+
+sourceSets.all {
+    val file = layout.buildDirectory.dir("generated/ksp/${name}/kotlin").get().asFile
+    if (file.exists()) {
+        java.srcDir(file)
+    }
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
+        // TODO Use K2 compiler if plugin support is added
         freeCompilerArgs = freeCompilerArgs - listOf("-Xuse-k2")
     }
 }
