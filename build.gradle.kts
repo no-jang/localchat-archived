1c5c2964b87fedaf8804c3a8cd60d081a33a457f
msengbusch <66240817+msengbusch@users.noreply.github.com>
2022-09-24 12:58:12 +0200

    fix(gradle): accept build scan terms of service automatically

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,9 @@ allprojects {
     group = "de.localchat"
     version = "0.1.0"
 }
+
+// Accept terms of service for publishing a gradle build scan
+extensions.findByName("gradleEnterprise")?.let {
+    setProperty("termsOfServiceUrl", "https://gradle.com/terms-of-service")
+    setProperty("termsOfServiceAgree", "yes")
+}
