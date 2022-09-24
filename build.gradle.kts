2358c4b9c9eb150995a515240f5aa33dbc9be214
msengbusch <66240817+msengbusch@users.noreply.github.com>
2022-09-24 13:23:38 +0200

    fix(gradle): accept build scan terms of service automatically

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,3 @@ allprojects {
     group = "de.localchat"
     version = "0.1.0"
 }
-
-// Accept terms of service for publishing a gradle build scan
-extensions.findByName("buildScan")?.let {
-    setProperty("termsOfServiceUrl", "https://gradle.com/terms-of-service")
-    setProperty("termsOfServiceAgree", "yes")
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,21 +1,30 @@
 import org.gradle.api.internal.FeaturePreviews.Feature
 
-rootProject.name = "localchat"
-
-// Enable features previews
-enableFeaturePreview(Feature.TYPESAFE_PROJECT_ACCESSORS.toString())
-enableFeaturePreview(Feature.STABLE_CONFIGURATION_CACHE.toString())
+//val ciMode = providers.environmentVariable("CI").map { it.toBoolean() }.get()
 
 // Extract build logic into own include Build
 pluginManagement {
     includeBuild("gradle/build-logic")
 }
 
-// Add a dependency catalog for dependencies used for executing the build
-dependencyResolutionManagement {
-    versionCatalogs {
-        create("build") {
-            from(files("gradle/build.versions.toml"))
+plugins {
+    // Apply this plugin to accept build scan terms of service
+    id("com.gradle.enterprise") version "3.11.1"
+}
+
+rootProject.name = "localchat"
+
+// Enable features previews
+enableFeaturePreview(Feature.TYPESAFE_PROJECT_ACCESSORS.toString())
+enableFeaturePreview(Feature.STABLE_CONFIGURATION_CACHE.toString())
+
+// Automatically accept build scan terms of service only if in CI
+if(System.getenv()["CI"] == "true") {
+    gradleEnterprise {
+        buildScan {
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+            publishAlways()
         }
     }
 }
