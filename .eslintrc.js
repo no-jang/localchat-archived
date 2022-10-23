66be455473c9a23d3f031a001980d801f8d0dbe3
msengbusch <marek.sengbusch@flg-online.de>
2022-10-23 13:43:36 +0200

    chore: move eslint to root project

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'turbo'],
+	plugins: ['svelte3', '@typescript-eslint'],
+	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	settings: {
+		'svelte3/typescript': () => require('typescript')
+	},
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 2020
+	},
+	env: {
+		browser: true,
+		es2017: true,
+		node: true
+	}
+};
