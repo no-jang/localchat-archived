24c6cf1c8bd8341793a0cd37da14a8094acbd70a
msengbusch <marek.sengbusch@flg-online.de>
2022-10-23 14:04:11 +0200

    style: apply prettier

--- modules/app/frontend/playwright.config.ts
+++ modules/app/frontend/playwright.config.ts
@@ -1,10 +1,10 @@
 import type { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
-  webServer: {
-    command: "pnpm run build && pnpm run preview",
-    port: 4173
-  }
+    webServer: {
+        command: "pnpm run build && pnpm run preview",
+        port: 4173
+    }
 };
 
 export default config;
--- modules/app/frontend/svelte.config.js
+++ modules/app/frontend/svelte.config.js
@@ -3,15 +3,15 @@ import preprocess from "svelte-preprocess";
 
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: preprocess(),
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: preprocess(),
 
-  kit: {
-    adapter: adapter({
-      pages: "dist"
-    })
-  }
+    kit: {
+        adapter: adapter({
+            pages: "dist"
+        })
+    }
 };
 
 export default config;
