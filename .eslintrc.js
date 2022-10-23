e3aaf780177aca27bbd7535a5ae72218be9ada1e
msengbusch <marek.sengbusch@flg-online.de>
2022-10-23 14:00:29 +0200

    style: apply prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,26 @@
 module.exports = {
-	parser: '@typescript-eslint/parser',
-	extends: ['plugin:@typescript-eslint/recommended', 'eslint:recommended', 'prettier', 'turbo'],
-	plugins: ['@typescript-eslint', 'prettier', 'svelte3'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript')
-	},
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020
-	},
-	env: {
-		browser: true,
-		es2017: true,
-		node: true
-	}
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "eslint:recommended",
+    "prettier",
+    "turbo",
+  ],
+  plugins: ["@typescript-eslint", "prettier", "svelte3"],
+  overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
+  settings: {
+    "svelte3/typescript": () => require("typescript"),
+  },
+  rules: {
+    "prettier/prettier": "error",
+  },
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2020,
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true,
+  },
 };
