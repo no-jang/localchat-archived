1aea511319494bd2e7fdade74c941e781c261de5
msengbusch <marek.sengbusch@flg-online.de>
2022-10-25 20:37:09 +0200

    feat: add astrojs

---
+++ modules/core/frontend/build.gradle.kts
@@ -0,0 +1,25 @@
+import com.github.gradle.node.pnpm.task.PnpmTask
+
+plugins {
+    base
+    id("com.github.node-gradle.node") version "3.5.0"
+}
+
+node {
+    download.set(false)
+}
+
+tasks {
+    val make = create<PnpmTask>("make") {
+        args.set(listOf("run", "build"))
+
+        inputs.dir("src")
+        inputs.dir("public")
+        outputs.dir("dist")
+        outputs.cacheIf { true }
+    }
+
+    getByName(BasePlugin.ASSEMBLE_TASK_NAME) {
+        dependsOn(make)
+    }
+}
